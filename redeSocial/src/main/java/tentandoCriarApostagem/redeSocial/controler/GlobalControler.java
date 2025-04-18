2467f713717204595bef2c7cfb6ace3924f939c8
Wesley <wesleyaraujoteixeira98@gmail.com>
2025-04-18 18:38:45 -0300

    enviando

--- redeSocial/src/main/java/tentandoCriarApostagem/redeSocial/controler/GlobalControler.java
+++ redeSocial/src/main/java/tentandoCriarApostagem/redeSocial/controler/GlobalControler.java
@@ -31,6 +31,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/redes")
 public class GlobalControler {
