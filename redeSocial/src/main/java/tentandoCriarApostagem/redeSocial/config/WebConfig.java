1b726928a0e8601c606fc0eafa341ed8d630d452
Wesley <wesleyaraujoteixeira98@gmail.com>
2025-05-14 01:36:42 -0300

    deploy do front domain

--- redeSocial/src/main/java/tentandoCriarApostagem/redeSocial/config/WebConfig.java
+++ redeSocial/src/main/java/tentandoCriarApostagem/redeSocial/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("social-azure-seven.vercel.app")
                         .allowedMethods("*");
             }
         };
