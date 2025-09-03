0f29d18aca7f0dfcc20ff33491420d2a8c2e4fbe
Wesley <wesleyaraujoteixeira98@gmail.com>
2025-09-03 11:26:24 -0300

    deploy

--- redeSocial/src/main/java/tentandoCriarApostagem/redeSocial/config/WebConfig.java
+++ redeSocial/src/main/java/tentandoCriarApostagem/redeSocial/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://rede-social-front-end-three.vercel.app/")
                         .allowedMethods("*");
             }
         };
--- redeSocial/src/main/java/tentandoCriarApostagem/redeSocial/security/SecurityConfig.java
+++ redeSocial/src/main/java/tentandoCriarApostagem/redeSocial/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("https://rede-social-front-end-three.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true); // se for usar cookies ou Authorization
